86bbc59da2c40b05d6657633fba33a88a27fc633
Huynh Minh Khanh <huynhminhkhanh@gmail.com>
2024-02-11 13:10:54 +0700

    Amazon_local_database_config

--- Backend/addToCart/src/main/java/com/example/addToCart/Service/ProductDetailsProxy.java
+++ Backend/addToCart/src/main/java/com/example/addToCart/Service/ProductDetailsProxy.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.addToCart.Entity.Product;
 
-@FeignClient(name="product-details", url="http://amazonproductdetails-1304216633.ap-southeast-1.elb.amazonaws.com")
+@FeignClient(name="product-details", url="http://localhost:8082")
 
 
 public interface ProductDetailsProxy {
